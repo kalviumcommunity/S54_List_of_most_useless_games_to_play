568e7ab75d5710d94b445d59beae682dcc119335
Max3796 <gurpreet.singh@kalvium.community>
2024-02-03 18:37:40 +0530

    Updated structure

---
+++ Client/src/components/Home.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='parent-home'>
+        <div className="home">
+            <img src="https://store-images.s-microsoft.com/image/apps.52397.13510798882606697.1816f804-e7fd-4295-9275-23dec3563baf.2ef7ec2d-2e37-489e-b6ac-b5f5e44d429c?mode=scale&q=90&h=1080&w=1920" alt="" />
+            <img src="https://store-images.s-microsoft.com/image/apps.52397.13510798882606697.1816f804-e7fd-4295-9275-23dec3563baf.2ef7ec2d-2e37-489e-b6ac-b5f5e44d429c?mode=scale&q=90&h=1080&w=1920" alt="" />
+            <img src="https://store-images.s-microsoft.com/image/apps.52397.13510798882606697.1816f804-e7fd-4295-9275-23dec3563baf.2ef7ec2d-2e37-489e-b6ac-b5f5e44d429c?mode=scale&q=90&h=1080&w=1920" alt="" />
+            <img src="https://store-images.s-microsoft.com/image/apps.52397.13510798882606697.1816f804-e7fd-4295-9275-23dec3563baf.2ef7ec2d-2e37-489e-b6ac-b5f5e44d429c?mode=scale&q=90&h=1080&w=1920" alt="" />
+            <img src="https://store-images.s-microsoft.com/image/apps.52397.13510798882606697.1816f804-e7fd-4295-9275-23dec3563baf.2ef7ec2d-2e37-489e-b6ac-b5f5e44d429c?mode=scale&q=90&h=1080&w=1920" alt="" />
+            <img src="https://store-images.s-microsoft.com/image/apps.52397.13510798882606697.1816f804-e7fd-4295-9275-23dec3563baf.2ef7ec2d-2e37-489e-b6ac-b5f5e44d429c?mode=scale&q=90&h=1080&w=1920" alt="" />
+            <img src="https://store-images.s-microsoft.com/image/apps.52397.13510798882606697.1816f804-e7fd-4295-9275-23dec3563baf.2ef7ec2d-2e37-489e-b6ac-b5f5e44d429c?mode=scale&q=90&h=1080&w=1920" alt="" />
+            <img src="https://store-images.s-microsoft.com/image/apps.52397.13510798882606697.1816f804-e7fd-4295-9275-23dec3563baf.2ef7ec2d-2e37-489e-b6ac-b5f5e44d429c?mode=scale&q=90&h=1080&w=1920" alt="" />
+            <img src="https://store-images.s-microsoft.com/image/apps.52397.13510798882606697.1816f804-e7fd-4295-9275-23dec3563baf.2ef7ec2d-2e37-489e-b6ac-b5f5e44d429c?mode=scale&q=90&h=1080&w=1920" alt="" />
+            <img src="https://store-images.s-microsoft.com/image/apps.52397.13510798882606697.1816f804-e7fd-4295-9275-23dec3563baf.2ef7ec2d-2e37-489e-b6ac-b5f5e44d429c?mode=scale&q=90&h=1080&w=1920" alt="" />
+            <img src="https://store-images.s-microsoft.com/image/apps.52397.13510798882606697.1816f804-e7fd-4295-9275-23dec3563baf.2ef7ec2d-2e37-489e-b6ac-b5f5e44d429c?mode=scale&q=90&h=1080&w=1920" alt="" />
+            <img src="https://store-images.s-microsoft.com/image/apps.52397.13510798882606697.1816f804-e7fd-4295-9275-23dec3563baf.2ef7ec2d-2e37-489e-b6ac-b5f5e44d429c?mode=scale&q=90&h=1080&w=1920" alt="" />
+            <img src="https://store-images.s-microsoft.com/image/apps.52397.13510798882606697.1816f804-e7fd-4295-9275-23dec3563baf.2ef7ec2d-2e37-489e-b6ac-b5f5e44d429c?mode=scale&q=90&h=1080&w=1920" alt="" />
+            <img src="https://store-images.s-microsoft.com/image/apps.52397.13510798882606697.1816f804-e7fd-4295-9275-23dec3563baf.2ef7ec2d-2e37-489e-b6ac-b5f5e44d429c?mode=scale&q=90&h=1080&w=1920" alt="" />
+            <img src="https://store-images.s-microsoft.com/image/apps.52397.13510798882606697.1816f804-e7fd-4295-9275-23dec3563baf.2ef7ec2d-2e37-489e-b6ac-b5f5e44d429c?mode=scale&q=90&h=1080&w=1920" alt="" />
+            <img src="https://store-images.s-microsoft.com/image/apps.52397.13510798882606697.1816f804-e7fd-4295-9275-23dec3563baf.2ef7ec2d-2e37-489e-b6ac-b5f5e44d429c?mode=scale&q=90&h=1080&w=1920" alt="" />
+            <img src="https://store-images.s-microsoft.com/image/apps.52397.13510798882606697.1816f804-e7fd-4295-9275-23dec3563baf.2ef7ec2d-2e37-489e-b6ac-b5f5e44d429c?mode=scale&q=90&h=1080&w=1920" alt="" />
+            <img src="https://store-images.s-microsoft.com/image/apps.52397.13510798882606697.1816f804-e7fd-4295-9275-23dec3563baf.2ef7ec2d-2e37-489e-b6ac-b5f5e44d429c?mode=scale&q=90&h=1080&w=1920" alt="" />
+            <img src="https://store-images.s-microsoft.com/image/apps.52397.13510798882606697.1816f804-e7fd-4295-9275-23dec3563baf.2ef7ec2d-2e37-489e-b6ac-b5f5e44d429c?mode=scale&q=90&h=1080&w=1920" alt="" />
+            <img src="https://store-images.s-microsoft.com/image/apps.52397.13510798882606697.1816f804-e7fd-4295-9275-23dec3563baf.2ef7ec2d-2e37-489e-b6ac-b5f5e44d429c?mode=scale&q=90&h=1080&w=1920" alt="" />
+            <img src="https://store-images.s-microsoft.com/image/apps.52397.13510798882606697.1816f804-e7fd-4295-9275-23dec3563baf.2ef7ec2d-2e37-489e-b6ac-b5f5e44d429c?mode=scale&q=90&h=1080&w=1920" alt="" />
+            <img src="https://store-images.s-microsoft.com/image/apps.52397.13510798882606697.1816f804-e7fd-4295-9275-23dec3563baf.2ef7ec2d-2e37-489e-b6ac-b5f5e44d429c?mode=scale&q=90&h=1080&w=1920" alt="" />
+            <img src="https://store-images.s-microsoft.com/image/apps.52397.13510798882606697.1816f804-e7fd-4295-9275-23dec3563baf.2ef7ec2d-2e37-489e-b6ac-b5f5e44d429c?mode=scale&q=90&h=1080&w=1920" alt="" />
+            <img src="https://store-images.s-microsoft.com/image/apps.52397.13510798882606697.1816f804-e7fd-4295-9275-23dec3563baf.2ef7ec2d-2e37-489e-b6ac-b5f5e44d429c?mode=scale&q=90&h=1080&w=1920" alt="" />
+            <img src="https://store-images.s-microsoft.com/image/apps.52397.13510798882606697.1816f804-e7fd-4295-9275-23dec3563baf.2ef7ec2d-2e37-489e-b6ac-b5f5e44d429c?mode=scale&q=90&h=1080&w=1920" alt="" />
+            <img src="https://store-images.s-microsoft.com/image/apps.52397.13510798882606697.1816f804-e7fd-4295-9275-23dec3563baf.2ef7ec2d-2e37-489e-b6ac-b5f5e44d429c?mode=scale&q=90&h=1080&w=1920" alt="" />
+            <img src="https://store-images.s-microsoft.com/image/apps.52397.13510798882606697.1816f804-e7fd-4295-9275-23dec3563baf.2ef7ec2d-2e37-489e-b6ac-b5f5e44d429c?mode=scale&q=90&h=1080&w=1920" alt="" />
+            <img src="https://store-images.s-microsoft.com/image/apps.52397.13510798882606697.1816f804-e7fd-4295-9275-23dec3563baf.2ef7ec2d-2e37-489e-b6ac-b5f5e44d429c?mode=scale&q=90&h=1080&w=1920" alt="" />
+            <img src="https://store-images.s-microsoft.com/image/apps.52397.13510798882606697.1816f804-e7fd-4295-9275-23dec3563baf.2ef7ec2d-2e37-489e-b6ac-b5f5e44d429c?mode=scale&q=90&h=1080&w=1920" alt="" />
+            <img src="https://store-images.s-microsoft.com/image/apps.52397.13510798882606697.1816f804-e7fd-4295-9275-23dec3563baf.2ef7ec2d-2e37-489e-b6ac-b5f5e44d429c?mode=scale&q=90&h=1080&w=1920" alt="" />
+            <img src="https://store-images.s-microsoft.com/image/apps.52397.13510798882606697.1816f804-e7fd-4295-9275-23dec3563baf.2ef7ec2d-2e37-489e-b6ac-b5f5e44d429c?mode=scale&q=90&h=1080&w=1920" alt="" />
+            <img src="https://store-images.s-microsoft.com/image/apps.52397.13510798882606697.1816f804-e7fd-4295-9275-23dec3563baf.2ef7ec2d-2e37-489e-b6ac-b5f5e44d429c?mode=scale&q=90&h=1080&w=1920" alt="" />
+            <img src="https://store-images.s-microsoft.com/image/apps.52397.13510798882606697.1816f804-e7fd-4295-9275-23dec3563baf.2ef7ec2d-2e37-489e-b6ac-b5f5e44d429c?mode=scale&q=90&h=1080&w=1920" alt="" />
+            <img src="https://store-images.s-microsoft.com/image/apps.52397.13510798882606697.1816f804-e7fd-4295-9275-23dec3563baf.2ef7ec2d-2e37-489e-b6ac-b5f5e44d429c?mode=scale&q=90&h=1080&w=1920" alt="" />
+        </div>
+    </div>
+  )
+}
---
+++ Client/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='nav' >
+        <nav className='navbar'>
+            <div className="left">
+                JoyPlay Adventure
+            </div>
+            <div className="right">
+                <h4>Login</h4>
+                <h4>SignUp</h4>
+            </div>
+        </nav>
+    </div>
+  )
+}
